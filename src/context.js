925e76c3ef7b69bf8537828aa27b665010a544a8
vigneshkaliyamoorthy <vigneshkaliyamoorthy@yahoo.com>
2019-11-05 20:47:47 +0530

    Cart Component is created and in progress

--- src/context.js
+++ src/context.js
@@ -9,7 +9,10 @@ class ProductProvider extends Component {
         detailProduct: detailProduct,
         cart: [],
         modalOpen: false,
-        modalProduct: detailProduct
+        modalProduct: detailProduct,
+        cartSubTotal: 0,
+        cartTax: 0,
+        cartTotal: 0
     }
 
     componentDidMount() {
@@ -69,6 +72,24 @@ class ProductProvider extends Component {
             return {modalOpen: false}
         })
     }
+
+    increment = (id) => {
+        console.log('Increment method');
+    }
+    decrement = (id) => {
+        console.log('decrement method');
+    }
+    removeItem = (id) => {
+        console.log('Item removed');
+    }
+    clearCart = () => {
+        console.log('cart was cleared');
+    }
+    addTotal = () => {
+        let subTotal = 0;
+        
+    }
+
     render() {
         return (
             <ProductContext.Provider value={{
@@ -76,7 +97,11 @@ class ProductProvider extends Component {
                 handleDetail: this.handleDetail,
                 addToCart: this.addToCart,
                 openModal: this.openModal,
-                closeModal: this.closeModal
+                closeModal: this.closeModal,
+                increment: this.increment,
+                decrement: this.decrement,
+                removeItem: this.removeItem,
+                clearCart: this.clearCart
             }}>
                 {this.props.children}
             </ProductContext.Provider>
